f21ec99e78fd1c6b2f32520571c50f964191c375
parthagrawalpg17-hub <parthagrawalpg17@gmail.com>
2026-09-13 12:29:42 +0530

    Delete ParthLauncher directory

--- ParthLauncher/gradlew.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-SET APP_HOME=%~dp0
-SET CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
-IF NOT EXIST "%CLASSPATH%" (
-  ECHO Gradle wrapper JAR is missing. Run in GitHub Actions or add gradle-wrapper.jar.
-  EXIT /B 1
-)
-java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
